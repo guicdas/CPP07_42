94ed65d8dcf87132408a3ad77e263f676348eedd
guicdas <silva.caldeira.guilherme@gmail.com>
2024-10-18 14:52:02 +0100

    done

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -20,5 +20,24 @@ int main( void ) {
 	std::cout << "swapped c and d!\n"<< "c = " << c << ", d = " << d << std::endl;
 	std::cout << "minimum( c, d ) = " << ::minimum( c, d ) << std::endl;
 	std::cout << "maximum( c, d ) = " << ::maximum( c, d ) << std::endl;
+	std::cout << std::endl;
 
+	float e = 5.5f;
+	float f = 20.7f;
+
+	std::cout << "e = " << e << ", f = " << f << std::endl;
+	::swap(e, f);
+	std::cout << "swapped e and f!\n"<< "e = " << e << ", f = " << f << std::endl;
+	std::cout << "minimum( e, f ) = " << ::minimum( e, f ) << std::endl;
+	std::cout << "maximum( e, f ) = " << ::maximum( e, f ) << std::endl;
+	std::cout << std::endl;
+
+	void *g = &a;
+	void *h = &b;
+
+	std::cout << "g = " << g << ", h = " << h << std::endl;
+	::swap(g, h);
+	std::cout << "swapped g and f!\n"<< "g = " << g << ", h = " << h << std::endl;
+	std::cout << "minimum( g, h ) = " << ::minimum( g, h ) << std::endl;
+	std::cout << "maximum( g, h ) = " << ::maximum( g, h ) << std::endl;
 }
